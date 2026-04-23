34d3d42fd5705d45860e805a04fc006fdd7f98f2
Sebastian Chacón <sebaschg21@gmail.com>
2026-04-23 15:15:04 -0500

    Arreglo del void VerificarEstado

--- Semana4_Ejercicio4/Semana4_Ejercicio4/Partida.cs
+++ Semana4_Ejercicio4/Semana4_Ejercicio4/Partida.cs
@@ -211,9 +211,9 @@ namespace Semana4_Ejercicio4
         }
 
 
-        public void VerificarEstadoHeroe()
+        public void VerificarEstado()
         {
-            if (!personaje.EstaVivo() || !personaje.BuenDesempeño())
+            if (!personaje.EstaVivo())
             {
                 Console.WriteLine("Aquí termina tu historia, suerte intentando ser un buen héroe la próxima vez");
             }
